3a58a40165d6cbccba2ff2e32b7be18fb491b092
Matias Franco <matias.n.franco@gmail.com>
2025-05-31 13:43:19 -0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background(30);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text('Hello, world!', width / 2, height / 2);
+}
+
+function draw() {
+  // No animation for now
+}
